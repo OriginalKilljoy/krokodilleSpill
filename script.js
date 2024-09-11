9b74779a5702c22606bba66bba5507b4736e5273
OriginalKilljoy <162512500+OriginalKilljoy@users.noreply.github.com>
2024-09-11 13:04:00 +0200

    finishedProduct

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+//Reset knapp - onClick, gir tallene
+//To tilfeldige tall - Variabler + math.random
+//Input felt - onChange
+//Funskjon for å sammenligne (if) - større enn, er lik eller mindre enn
+//Submit knapp - sjekke om det er skrevet inn riktig og oppdatere poengsum
+//Gi poeng, riktig svar +1 (++), feil er -1(--)
+
 let firstNum = 0;
 let secondNum = 0;
+let answer = '';
+let points = 0;
 
 function reset(){
     updateNumbers();
@@ -13,5 +22,34 @@ function updateNumbers(){
 }
 
 function getRandomNum(){
-    return Math.round(Math.random() * 10);
+    return Math.floor(Math.random() * 10);
+}
+
+function saveInput(input){
+    answer = input;
+}
+
+function rightAnswer(){
+    points++
+    document.getElementById("points").innerHTML = points;
+}
+
+function wrongAnswer(){
+    points--
+    document.getElementById("points").innerHTML = points;
+}
+
+function compareNum(){
+    if(answer == '=' && firstNum == secondNum){
+        rightAnswer()
+    }
+    else if(answer == '<' && firstNum < secondNum){
+        rightAnswer()
+    }
+    else if(answer == '>' && firstNum > secondNum){
+        rightAnswer()
+    }
+    else{
+        wrongAnswer()
+    }
 }
