f1fe78b88cf89ae02517098b002f94b29851b5a7
OriginalKilljoy <162512500+OriginalKilljoy@users.noreply.github.com>
2024-09-12 11:10:25 +0200

    With Notes

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-//Reset knapp - onClick, gir tallene
-//To tilfeldige tall - Variabler + math.random
-//Input felt - onChange
-//Funskjon for å sammenligne (if) - større enn, er lik eller mindre enn
-//Submit knapp - sjekke om det er skrevet inn riktig og oppdatere poengsum
-//Gi poeng, riktig svar +1 (++), feil er -1(--)
+//Reset knapp - onClick, gir tallene DONE
+//To tilfeldige tall - Variabler + math.random DONE
+//Input felt - onChange DONE
+//Funskjon for å sammenligne (if) - større enn, er lik eller mindre enn DONE
+//Submit knapp - sjekke om det er skrevet inn riktig og oppdatere poengsum DONE
+//Gi poeng, riktig svar +1 (++), feil er -1(--) DONE
 
 let firstNum = 0;
 let secondNum = 0;
 let answer = '';
 let points = 0;
 
+//Koblet opp mot reset knapp (index-fila), og kjører updateNumbers() for å gi nye tall 
 function reset(){
     updateNumbers();
 }
-
+//Henter informasjonen fra getRandomNum() og legger disse tallene inni variablene firstNum og secondNum, som blir lagt inn i index fila
 function updateNumbers(){
     firstNum = getRandomNum();
     secondNum = getRandomNum();
@@ -21,24 +22,29 @@ function updateNumbers(){
     document.getElementById("secondNum").innerHTML = secondNum;
 }
 
+//Gir oss tilfeldige tall fra 1-9
 function getRandomNum(){
     return Math.floor(Math.random() * 10);
 }
 
+//Lagrer svaret vi gir i input feltet
 function saveInput(input){
     answer = input;
 }
 
+//Gir oss 1 poeng hvis vi får riktig svar
 function rightAnswer(){
     points++
     document.getElementById("points").innerHTML = points;
 }
 
+//Trekker fra 1 poeng hvis vi får feil svar
 function wrongAnswer(){
     points--
     document.getElementById("points").innerHTML = points;
 }
 
+//Sjekker om svaret vårt stemmer overens med det som er riktig ift nummerne vi har fått.
 function compareNum(){
     if(answer == '=' && firstNum == secondNum){
         rightAnswer()
